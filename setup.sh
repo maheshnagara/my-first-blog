c87e0ffe19f0f1edf3bfff6e7e86d9cac37e3809
Mahesh Nagarajan <126008576+maheshnagara@users.noreply.github.com>
2023-02-21 12:25:47 -0600

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+TUTORIAL_HOME=${HOME}/src/djangogirls
+USER=$(whoami)
+UID=$(id -u "${USER}")
+GID=$(id -g "${USER}")
+
+echo "What's your GitHub username?"
+read -r github_username
+
+echo "What's your GitHub email?"
+read -r github_email
+
+mkdir -p "${TUTORIAL_HOME}"
+cd "${TUTORIAL_HOME}" || return
+
+docker run --rm -v "${PWD}":/root/src/djangogirls \
+  gsong/djangogirls-app \
+  /bin/bash -c "git clone https://github.com/${github_username}/my-first-blog.git . ; chown -R '${UID}:${GID}' /root/src/djangogirls"
+
+cat << EOF > .env
+GITHUB_USERNAME=${github_username}
+GITHUB_EMAIL=${github_email}
+USER=${USER}
+UID=${UID}
+GID=${GID}
+EOF
